2d7e210a92c5a9ce8dd824037fc7fca515795b75
physacco <physacco@gmail.com>
2013-03-24 23:18:39 +0800

    refactor xrelay and socks5 with XReadWriter

--- src/xrelay.go
+++ src/xrelay.go
@@ -18,6 +18,57 @@ var (
     CIPHER   []byte  // XOR cipher key, default is nil
 )
 
+type XReader struct {
+    io.Reader
+    Cipher []byte
+    Position uint64
+}
+
+func (rdr *XReader) Read(p []byte) (n int, err error) {
+    n, err = rdr.Reader.Read(p)
+    keylen := uint64(len(rdr.Cipher))
+    if keylen > 0 {
+        for i := 0; i < n; i++ {
+            keypos := rdr.Position % keylen
+            p[i] ^= rdr.Cipher[keypos]
+            rdr.Position += 1
+        }
+    }
+    return
+}
+
+type XWriter struct {
+    io.Writer
+    Cipher []byte
+    Position uint64
+}
+
+func (wtr *XWriter) Write(p []byte) (n int, err error) {
+    plen := len(p)
+    buf := make([]byte, plen)
+    keylen := uint64(len(wtr.Cipher))
+    if keylen > 0 {
+        for i := 0; i < plen; i++ {
+            keypos := wtr.Position % keylen
+            buf[i] = p[i] ^ wtr.Cipher[keypos]
+            wtr.Position += 1
+        }
+    }
+    n, err = wtr.Writer.Write(buf)
+    return
+}
+
+type XReadWriter struct {
+    *XReader
+    *XWriter
+}
+
+func newXReadWriter(rdwtr io.ReadWriter, cipher []byte) XReadWriter {
+    reader := XReader{Reader: rdwtr, Cipher: cipher}
+    writer := XWriter{Writer: rdwtr, Cipher: cipher}
+    return XReadWriter{&reader, &writer}
+}
+
 func isUseOfClosedConn(err error) bool {
     operr, ok := err.(*net.OpError)
     return ok && operr.Err.Error() == "use of closed network connection"
@@ -28,8 +79,6 @@ func iobridge(src io.Reader, dst io.Writer, shutdown chan bool) {
         shutdown <- true
     }()
 
-    keylen, keypos := len(CIPHER), 0
-
     buf := make([]byte, 8192)
     for {
         n, err := src.Read(buf)
@@ -38,16 +87,6 @@ func iobridge(src io.Reader, dst io.Writer, shutdown chan bool) {
                 log.Printf("error reading %s: %s\n", src, err)
             }
             break
-        }
-
-        if keylen > 0 {
-            for i := 0; i < n; i++ {
-                buf[i] ^= CIPHER[keypos]
-                keypos += 1
-                if keypos >= keylen {
-                    keypos -= keylen
-                }
-            }
         }
 
         _, err = dst.Write(buf[:n])
@@ -83,6 +122,14 @@ func handleConnection(frontconn net.Conn) {
         log.Println("DISCONNECTED backend", backconn, backaddr)
     }()
 
+    if len(CIPHER) > 0 {
+        performConnect(frontconn, newXReadWriter(backconn, CIPHER))
+    } else {
+        performConnect(frontconn, backconn)
+    }
+}
+
+func performConnect(frontconn, backconn io.ReadWriter) {
     shutdown := make(chan bool)
     go iobridge(frontconn, backconn, shutdown)
     go iobridge(backconn, frontconn, shutdown)
